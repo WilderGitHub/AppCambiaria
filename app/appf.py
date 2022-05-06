0cc11e5a5d5cdf82220d649479dcf786edf80e1b
WilderGitHub <wilder_serdan@hotmail.com>
2022-05-06 16:22:13 -0400

    seguimos refactorizando

--- app/appf.py
+++ app/appf.py
@@ -1,10 +1,35 @@
+import PySimpleGUI as sg
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
 from junciones import *
-
 #Archivo de parámetros, movimientos y palabras clave
 parametros = "parametros.xlsx"
-archivo="gtes10ene.xls"
 #Aqui empezaría la onda
-print ("leyendo archivo ", leer(archivo).head(3))
+
+# tema
+sg.theme('LightGrey1')
+# el diseño
+
+layout = [[sg.T("")],
+          losInputs("GEF: ","-GEF2-","GEF",30), losInputs("GOM: ","-GOM2-","GOM",26),
+          losInputs("GOI: ","-GOI2-","GOI",34), losInputs("GTES: ","-GTESS-","GTES",22),
+          [sg.Button("Procesar", pad=((350, 0), 30), font='Arial 12', button_color=('firebrick3'))]]
+# Creamos la ventana
+window = sg.Window('Balanza Cambiaria', layout, size=(750, 250))
+# escuchamos los eventos
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Exit":
+        break
+    elif event == "Procesar":
+        bdgef = leer(os.path.abspath(values["GEF"])) # lee una ruta
+        bdgom = leer(os.path.abspath(values["GOM"]))
+        bdgoi = leer(os.path.abspath(values["GOI"]))
+        bdgtes = leer(os.path.abspath(values["GTES"]))
+        #concatenamos todo
+        bdBruto = pd.concat([bdgtes, bdgoi, bdgom, bdgef], ignore_index=True)
+        # todo a minusculas
+        bdBruto.columns = map(str.lower, bdBruto.columns)
+        # reducimos columnas 
+        print("bruta",bdBruto.shape)
+        print ("reudcida ", reducirColumnas(os.path.abspath(values["GEF"]),bdBruto).shape)
--- app/junciones.py
+++ app/junciones.py
@@ -1,4 +1,4 @@
-#funciones
+import PySimpleGUI as sg#funciones
 import os
 from dbfread import DBF
 import pandas as pd
@@ -24,12 +24,36 @@ def getParametros(archivo,hoja=""):
     x = pd.read_excel(archivo, sheet_name=hoja)    
     return x
 
+def getExtension(archivo):
+    extension = os.path.splitext(archivo)[1]
+    return extension
+    
 def leer(nombreArchivo):
-    extension = os.path.splitext(nombreArchivo)[1]
-    if extension == '.dbf':
+    #extension = os.path.splitext(nombreArchivo)[1]
+    if getExtension(nombreArchivo) == '.dbf':
         table = DBF(nombreArchivo, encoding='latin', load=True)
         return pd.DataFrame(iter(table))
     else:
         #para los que no son dbf
         return pd.read_excel(nombreArchivo)
-# funciones
+
+def losInputs(nombreCampo,inputKey,fileKey, espacio):
+    fila=[sg.Text(nombreCampo),
+    sg.Input(key=inputKey, 
+        change_submits=True, 
+        pad=((espacio, 10), 5), 
+        size=(80, 1)),
+    sg.FileBrowse(key=fileKey)]
+    return fila
+
+def reducirColumnas (nombreArchivo,ddff):
+        if getExtension (nombreArchivo) == '.dbf':
+            bdReducida = ddff.loc[:, ('fecha_dia', 'nro_centro', 'cve_tipo_c', 'glosa_reng', 'cve_debe_h', 'monto_mo',
+                                     'cod_moneda', 'cod_movimi', 'nom_movimi',
+                                         'monto_mn', 'glosa_comp', 'nro_compro', 'cod_mayor')]
+        else:
+
+            bdReducida = ddff.loc[:, ('fecha_dia', 'nro_centro', 'cve_tipo_comprob', 'glosa_reng', 'cve_debe_haber', 'monto_mo',
+                                     'cod_moneda', 'cod_movimiento', 'nom_movimiento',
+                                         'monto_mn', 'glosa_comprob', 'nro_comprob', 'cod_mayor')]
+        return bdReducida
